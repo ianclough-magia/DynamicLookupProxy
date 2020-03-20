b13be78a0cd259a4f77f1d0ce60f98d65e9ef95f
Ian Clough <ian.clough@magiaconsulting.com>
2020-03-20 19:52:11 +0000

    Initial commit

---
+++ DynamicLookupProxy/Controllers/DynamicLookupController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace DynamicLookupProxy.Controllers
+{
+    [ApiController]
+    [Route("api/lookup")]
+    public class DynamicLookupController : ControllerBase
+    {
+
+        HttpClient client = new HttpClient();
+        
+        [HttpGet()]
+        public ActionResult<List<TextValue>> GetLookup(string query, string valueBinding, string textBinding)
+        {
+            Console.WriteLine("GetLookup");
+            Console.WriteLine("Query: " + query);
+            Console.WriteLine("ValueBinding: " + valueBinding);
+            Console.WriteLine("TextBinding: " + textBinding);
+            
+            List<TextValue> textValues = new List<TextValue>();
+
+            try
+            {
+                Task<HttpResponseMessage> responseTask =
+                    client.GetAsync("http://localhost:5000/api/lookup/ords/" + query);
+                HttpResponseMessage responseMessage = responseTask.Result;
+                string responseContent = responseMessage.Content.ReadAsStringAsync().Result;
+
+                JObject jobject = JObject.Parse(responseContent);
+
+                JArray jarray = (JArray) jobject["items"];
+                foreach (JObject item in jarray)
+                {
+                    if (textBinding != null)
+                    {
+                        textValues.Add(new TextValue(item[valueBinding].ToString(), item[textBinding].ToString()));
+                    }
+                    else
+                    {
+                        textValues.Add(new TextValue(item[valueBinding].ToString(), item[valueBinding].ToString()));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            return textValues;
+        }
+
+        private List<TextValue> GetOrganisations(string filter)
+        {
+            Console.WriteLine("GetOrganisations");
+            List<TextValue> organisations = new List<TextValue>();
+            organisations.Add(new TextValue("BEIS", "BEIS"));
+            organisations.Add(new TextValue("DIT", "DIT"));
+            organisations.Add(new TextValue("UKSA", "UKSA"));
+            return organisations;
+        }
+
+        private List<TextValue> GetCostCentres(string organisation)
+        {
+            Console.WriteLine("GetCostCentres");
+            List<TextValue> costCentres = new List<TextValue>();
+            if (organisation == "BEIS")
+            {
+                costCentres.Add(new TextValue("BEIS-123", "BEIS-123"));
+                costCentres.Add(new TextValue("BEIS-456", "BEIS-456"));
+                costCentres.Add(new TextValue("BEIS-789", "BEIS-789"));
+            } else if (organisation == "DIT")
+            {
+                costCentres.Add(new TextValue("DIT-111", "DIT-111"));
+                costCentres.Add(new TextValue("DIT-222", "DIT-222"));
+                costCentres.Add(new TextValue("DIT-333", "DIT-333"));
+            }
+            else if (organisation == "UKSA")
+            {
+                costCentres.Add(new TextValue("987", "987"));
+                costCentres.Add(new TextValue("654", "654"));
+                costCentres.Add(new TextValue("321", "321"));
+            }
+
+            return costCentres;
+        }
+
+        private List<TextValue> GetPositions(string costCentre)
+        {
+            Console.WriteLine("GetPositions");
+            List<TextValue> positions = new List<TextValue>();
+            if (costCentre == "BEIS-123")
+            {
+                positions.Add(new TextValue("123-xxx", "123-xxx"));
+                positions.Add(new TextValue("123-yyy", "123-yyy"));
+                positions.Add(new TextValue("123-zzz", "123-zzz"));
+            }
+            if (costCentre == "BEIS-456")
+            {
+                positions.Add(new TextValue("456-xxx", "456-xxx"));
+                positions.Add(new TextValue("456-yyy", "456-yyy"));
+                positions.Add(new TextValue("456-zzz", "456-zzz"));
+            }
+            if (costCentre == "BEIS-789")
+            {
+                positions.Add(new TextValue("789-xxx", "789-xxx"));
+                positions.Add(new TextValue("789-yyy", "789-yyy"));
+                positions.Add(new TextValue("789-zzz", "789-zzz"));
+            }
+            if (costCentre == "DIT-111")
+            {
+                positions.Add(new TextValue("111-xxx", "111-xxx"));
+                positions.Add(new TextValue("111-yyy", "111-yyy"));
+                positions.Add(new TextValue("111-zzz", "111-zzz"));
+            }
+            if (costCentre == "DIT-222")
+            {
+                positions.Add(new TextValue("222-xxx", "222-xxx"));
+                positions.Add(new TextValue("222-yyy", "222-yyy"));
+                positions.Add(new TextValue("222-zzz", "222-zzz"));
+            }
+            if (costCentre == "DIT-333")
+            {
+                positions.Add(new TextValue("333-xxx", "333-xxx"));
+                positions.Add(new TextValue("333-yyy", "333-yyy"));
+                positions.Add(new TextValue("333-zzz", "333-zzz"));
+            }
+            if (costCentre == "987")
+            {
+                positions.Add(new TextValue("987-xxx", "987-xxx"));
+                positions.Add(new TextValue("987-yyy", "987-yyy"));
+                positions.Add(new TextValue("987-zzz", "987-zzz"));
+            }
+            if (costCentre == "654")
+            {
+                positions.Add(new TextValue("654-xxx", "654-xxx"));
+                positions.Add(new TextValue("654-yyy", "654-yyy"));
+                positions.Add(new TextValue("654-zzz", "654-zzz"));
+            }
+            if (costCentre == "321")
+            {
+                positions.Add(new TextValue("321-xxx", "321-xxx"));
+                positions.Add(new TextValue("321-yyy", "321-yyy"));
+                positions.Add(new TextValue("321-zzz", "321-zzz"));
+            }
+            return positions;
+        }
+    }
+
+    public class TextValue
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public TextValue(string text, string value)
+        {
+            Text = text;
+            Value = value;
+        }
+    }
+}
---
+++ DynamicLookupProxy/Controllers/OrdsStubController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicLookupProxy.Controllers
+{
+    [ApiController]
+    [Route("api/lookup/ords")]
+    public class OrdsStubController : ControllerBase
+    {
+
+        [HttpGet("organisations")]
+        public ResponseItems Organisations()
+        {
+            ResponseItems responseItems = new ResponseItems(new List<object>()
+            {
+                new OrdsOrganisation("1000", "UKGV 100"),
+                new OrdsOrganisation("1001", "UKSBS 101")
+            });
+            return responseItems;
+        }
+        
+        [HttpGet("organisations/{organisation}/costcentres")]
+        public ResponseItems CostCentres(string organisation)
+        {
+            ResponseItems responseItems = new ResponseItems(new List<object>()
+            {
+                new OrdsCostCentre("1000", "UKGV 100", "107988"),
+                new OrdsCostCentre("1000", "UKGV 100", "105739")
+            });
+            return responseItems;
+        }
+        
+        [HttpGet("organisations/{organisation}/costcentres/{costcentre}/positions")]
+        public ResponseItems Positions(string organisation, string costcentre)
+        {
+            ResponseItems responseItems = new ResponseItems(new List<object>()
+            {
+                new OrdsPosition("1000", "UKGV 100", "107988", "BIS-1.1.2.107988"),
+                new OrdsPosition("1000", "UKGV 100", "107988", "SIP-1.2.5.107988")
+            });
+            return responseItems;
+        }
+        
+    }
+
+    public class ResponseItems
+    {
+        public List<object> Items { get; }
+
+        public ResponseItems(List<object> items)
+        {
+            Items = items;
+        }
+    }
+
+    public class OrdsOrganisation
+    {
+        public string Org_id { get; }
+        public string Org_name { get; }
+
+        public OrdsOrganisation(string org_id, string org_name)
+        {
+            Org_id = org_id;
+            Org_name = org_name;
+        }
+    }
+
+    public class OrdsCostCentre
+    {
+        public string Org_id { get; }
+        public string Org_name { get; }
+        public string Cost_centre { get; }
+        public OrdsCostCentre(string org_id, string org_name, string cost_centre)
+        {
+            Org_id = org_id;
+            Org_name = org_name;
+            Cost_centre = cost_centre;
+        }
+    }
+    
+    public class OrdsPosition
+    {
+        public string Org_id { get; }
+        public string Org_name { get; }
+        public string Cost_centre { get; }
+        public string Position { get; }
+        public OrdsPosition(string org_id, string org_name, string cost_centre, string position)
+        {
+            Org_id = org_id;
+            Org_name = org_name;
+            Cost_centre = cost_centre;
+            Position = position;
+        }
+    }
+}
